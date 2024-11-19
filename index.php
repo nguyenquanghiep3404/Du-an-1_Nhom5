5f2f776bb17bdf5822325c16749ae5c3e0a7b742
dothanhbinh205 <dothanhbinh18112005@gmail.com>
2024-11-19 10:53:53 +0700

    #10-login

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
 require_once "./commons/env.php";
 require_once "./commons/function.php";
 // Kết nối với model
+require_once './models/Product.php';
 require_once './models/categoryModel.php';
+require_once './models/loginModel.php';
 // Kết nối Controller
 require_once './controllers/admin/ProductAdminController.php';
 require_once './controllers/admin/categoryControllers.php   ';
+require_once './controllers/admin/loginController.php';
 $action = isset($_GET["action"]) ? $_GET["action"] :'admin';
 $productAdmin = new ProductAdminController();
 $categoryAdmin = new categoryControllers();
