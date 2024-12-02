6904ca1c67014789b5ddbf8bff8abf2383c6914d
nguyenquanghiep3404 <nguyenquanghiep3404@gmail.com>
2024-12-02 23:03:27 +0700

    #49-hoan thien chuc nang tim kiem

--- views/client/layout/modalPoduct.php
+++ views/client/layout/modalPoduct.php
@@ -1,4 +1,4 @@
-<?php var_dump($product) ?>
+<!-- <?php var_dump($product) ?> -->
     <!-- modal area start-->
     <div class="modal fade" id="modal_box" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
